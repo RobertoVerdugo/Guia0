e705b5f80d2da0e7e3d0030adaa4dd77a57fcb89
Felipe Barrientos Rojas <64845675+Felipe991@users.noreply.github.com>
2020-05-06 15:34:01 -0400

    Add files via upload

--- guia0.c
+++ guia0.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #define limite 2147483647
+
 int factorial_n_iterativo(int n);
+int factorial_n_recursivo(int n);
+int calcular_tamano(int n,int *tam_resultado);
+void factorial_2 (int n,int tam_resultado,int resultado[(tam_resultado)]);
 
 void main(){
 	int n;
@@ -9,6 +14,15 @@ void main(){
 	printf("\nEl factorial de %i es : %i\n", n,factorial_n_iterativo(n));
 	printf("\nEl factorial de %i es : %i\n", n,factorial_n_recursivo(n));
 	
+	int tam_resultado = 0;
+	
+	calcular_tamano(n,&tam_resultado);
+	
+	int resultado[tam_resultado];
+	printf("\nTam resultado vale = %i \n",tam_resultado);
+	
+	
+	factorial_2 (n,tam_resultado,resultado);
 	
 }
 
@@ -33,4 +47,34 @@ int factorial_n_recursivo(int n){
 	else{
 		return n*factorial_n_recursivo(n-1);
 	}
+}
+
+int calcular_tamano(int n,int *tam_resultado){
+	int	aux = (factorial_n_iterativo(n));
+		
+	while(aux>=1){
+		aux = aux/10;
+		(*tam_resultado)++;
+	}
+	return (*tam_resultado);
+}
+
+void factorial_2 (int n,int tam_resultado,int resultado[(tam_resultado)]){
+	int aux = (factorial_n_iterativo(n)),tope=tam_resultado,i=0,j=0;
+	
+	while(tam_resultado != 0){
+		int divisor = pow(10,tam_resultado-1);
+		printf("\nEl divisor es = %i,Aux tiene el valor de = %i \n",divisor,aux);
+		resultado[j] = aux/divisor;
+		aux = aux-(resultado[j]*divisor);
+		tam_resultado--;	
+		j++;
+	}
+	
+	printf("\n\n");
+	while(i<tope){
+		printf(" %i ",resultado[i]);
+		i++;
+	}
+	printf("\n\n");
 }
